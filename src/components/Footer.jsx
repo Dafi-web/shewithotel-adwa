db4a40e07e5aa5ce37889161abae6fd7354447c1
Your name <your.email@yourmailserver.com>
2025-04-03 12:31:28 +0200

    new footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-
 const Footer = () => {
-  return <footer>&copy; 2025 ShewitHotel Adwa. All rights reserved.
-    <p><i>designed by:</i>Dawit</p>
-  </footer>;
-  
+  return (
+    <footer>
+      &copy; 2025 ShewitHotel Adwa. All rights reserved.
+      <p><i>Designed by:</i> Dawit | <strong>Dafi-Tech</strong></p>
+      <p>
+        Need a website? Contact us at:  
+        <a href="mailto:wediabrhana@gmail.com"> wediabrhana@gmail.com</a>
+      </p>
+      <p>የምትፈልጉትን ዌብሳይት አንሰራለን ፡ ያናግሩን</p>
+    </footer>
+  );
 };
 
 export default Footer;
